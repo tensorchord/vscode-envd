c4c52eec6ef9aabc554c03095ad7b9b9aedc2ea5
cutecutecat <starkind1997@gmail.com>
2023-01-11 09:55:03 +0800

    implement VSCode style logger
    Signed-off-by: cutecutecat <starkind1997@gmail.com>

--- src/envd-lsp-client.ts
+++ src/envd-lsp-client.ts
@@ -12,153 +12,177 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import net = require("net")
-import path from "path"
-import process from "process"
-import { ChildProcess, spawn } from "child_process"
+import net from 'net';
+import path from 'path';
+import process from 'process';
+import {type ChildProcess, spawn} from 'child_process';
 import {
-  commands,
-  Disposable,
-  ExtensionContext,
-  OutputChannel,
-  window,
-  workspace,
-} from "vscode"
+	commands,
+	type Disposable,
+	type ExtensionContext,
+	type OutputChannel,
+	window,
+	workspace,
+} from 'vscode';
 import {
-  LanguageClient,
-  LanguageClientOptions,
-  StreamInfo,
-} from "vscode-languageclient/node"
+	LanguageClient,
+	type LanguageClientOptions,
+	type StreamInfo,
+} from 'vscode-languageclient/node';
 
-import { PlaceholderErrorHandler, EnvdErrorHandler } from "./error-handlers"
-import { getServerPort, getTrace, Port } from "./config"
-// import { checkEnvdVersion } from "./envd-version"
+import {PlaceholderErrorHandler, EnvdErrorHandler} from './error-handlers';
+import {getServerPort, getTrace, type Port} from './config';
+import {Module} from './logger';
 
-const extensionLang = "envd"
-const extensionName = "envd"
-const maxRestartCount = 5
-const envdUnavailableNotification = "envd language server could not be started"
-const envdUnavailableMessage =
-  "Could not find a version of envd to use with the envd extension. " +
-  "Please visit https://envd.tensorchord.ai/docs/get-started to install envd." +
-  "Autocomplete will not function without a compatible version of envd installed."
+const extensionLang = 'envd';
+const extensionName = 'envd';
+const maxRestartCount = 5;
+const envdUnavailableNotification = 'envd language server could not be started';
+const envdUnavailableMessage
+    = 'Could not find a version of envd to use with the envd extension. '
+    + 'Please visit https://envd.tensorchord.ai/docs/get-started to install envd.'
+    + 'Autocomplete will not function without a compatible version of envd installed.';
 
 export class EnvdLspClient extends LanguageClient {
-  private _usingDebugServer = false
+	static clientOptions(ch: OutputChannel): LanguageClientOptions {
+		return {
+			documentSelector: [{scheme: 'file', language: extensionLang}],
+			synchronize: {
+				// Notify the server about file changes to relevant files contained in the workspace
+				fileEvents: workspace.createFileSystemWatcher('**/*.envd'),
+			},
+			outputChannel: ch,
+			traceOutputChannel: ch,
+			errorHandler: new PlaceholderErrorHandler(),
+		};
+	}
 
-  public constructor(private context: ExtensionContext, ch: OutputChannel) {
-    super(
-      extensionLang,
-      extensionName,
-      () => this.startServer(),
-     EnvdLspClient.clientOptions(ch)
-    )
-    this.registerCommands()
-    this.installErrorHandler()
-  }
+	private _usingDebugServer = false;
 
-  static clientOptions(ch: OutputChannel): LanguageClientOptions {
-    return {
-      documentSelector: [{ scheme: "file", language: extensionLang }],
-      synchronize: {
-        // Notify the server about file changes to relevant files contained in the workspace
-        fileEvents: workspace.createFileSystemWatcher("**/*.envd"),
-      },
-      outputChannel: ch,
-      traceOutputChannel: ch,
-      errorHandler: new PlaceholderErrorHandler(),
-    }
-  }
+	public constructor(private readonly context: ExtensionContext, ch: OutputChannel) {
+		super(
+			extensionLang,
+			extensionName,
+			async () => this.startServer(),
+			EnvdLspClient.clientOptions(ch),
+		);
+		this.registerCommands();
+		this.installErrorHandler();
+	}
 
-  public get usingDebugServer() {
-    return this._usingDebugServer
-  }
+	public get usingDebugServer() {
+		return this._usingDebugServer;
+	}
 
-  public start(): Disposable {
-    const disp = super.start()
-    this.info("envd LSP started")
-    return disp
-  }
+	public start(): Disposable {
+		const disp = super.start();
+		this.info('envd LSP started');
+		return disp;
+	}
 
-  public registerCommands() {
-    this.context.subscriptions.push(
-      commands.registerCommand("envd.restartServer", () => {
-        this.info("Restarting server")
-        this.restart()
-      })
-    )
-  }
+	public registerCommands() {
+		this.context.subscriptions.push(
+			commands.registerCommand('envd.restartServer', () => {
+				this.info('Restarting server');
+				this.restart();
+			}),
+		);
+	}
 
-  public restart(): void {
-    this.stop()
-      .catch((e) => this.warn(e))
-      .then(() => this.start())
-  }
+	public restart(): void {
+		void this.stop()
+			.catch(e => {
+				this.warn(e);
+			})
+			.then(() => this.start());
+	}
 
-  private async startServer(): Promise<ChildProcess | StreamInfo> {
-    const port = await this.checkForDebugLspServer()
-    if (port) {
-      this.info("Connect to debug server")
-      this._usingDebugServer = true
-      this.outputChannel.show(true)
-      const socket = net.connect({ host: "127.0.0.1", port })
-      return { writer: socket, reader: socket }
-    }
+	public info(message: string) {
+		super.info(`${Module.LSP} ` + message);
+	}
 
-    var envdPath = path.join(__dirname, "../bin/envd-lsp_Linux_x86_64")
+	public warn(message: string) {
+		super.warn(`${Module.LSP} ` + message);
+	}
 
-    try {
-      switch (process.platform) {
-        case "darwin":
-          envdPath = path.join(__dirname, "../bin/envd-lsp_Darwin_x86_64")
-      }
-      const args = []
-      this.info("Starting child process")
-      const trace = getTrace()
-      switch (trace) {
-        case "debug":
-          this.outputChannel.show(true)
-          args.push("--debug")
-          break
-      }
-      args.push("lsp")
-      return spawn(envdPath, args)
-    } catch (e) {
-      this.warn(envdUnavailableMessage)
-      this.outputChannel.show()
-      window.showErrorMessage(envdUnavailableNotification)
-      throw e.toString()
-    }
-  }
+	public error(message: string) {
+		super.error(`${Module.LSP} ` + message);
+	}
 
-  private async checkForDebugLspServer(): Promise<Port> {
-    const port = getServerPort()
-    if (!port) {
-      return null
-    }
-    return new Promise((resolve) => {
-      const checkListen = () => {
-        var server = net.createServer()
-        server.on("error", () => resolve(port))
-        server.on("listening", () => {
-          server.close()
-          resolve(null)
-        })
-        server.listen(port, "127.0.0.1")
-      }
+	private async startServer(): Promise<ChildProcess | StreamInfo> {
+		const port = await this.checkForDebugLspServer();
+		if (port) {
+			this.info('Connect to debug server');
+			this._usingDebugServer = true;
+			this.outputChannel.show(true);
+			const socket = net.connect({host: '127.0.0.1', port});
+			return {writer: socket, reader: socket};
+		}
 
-      if (this.usingDebugServer) {
-        // wait for server to restart
-        setTimeout(checkListen, 2500)
-      } else {
-        checkListen()
-      }
-    })
-  }
+		let envdPath = path.join(__dirname, '../bin/envd-lsp_Linux_x86_64');
 
-  private installErrorHandler() {
-    const placeholder = this.clientOptions
-      .errorHandler as PlaceholderErrorHandler
-    placeholder.delegate = new EnvdErrorHandler(this, maxRestartCount)
-  }
+		try {
+			switch (process.platform) {
+				case 'darwin':
+					envdPath = path.join(__dirname, '../bin/envd-lsp_Darwin_x86_64');
+					break;
+				default:
+					break;
+			}
+
+			const args: string[] = [];
+			this.info('Starting child process');
+			const trace = getTrace();
+			switch (trace) {
+				case true:
+					this.outputChannel.show(true);
+					args.push('--debug');
+					break;
+				default:
+					break;
+			}
+
+			args.push('lsp');
+			return spawn(envdPath, args);
+		} catch (e) {
+			this.warn(envdUnavailableMessage);
+			this.outputChannel.show();
+			void window.showErrorMessage(envdUnavailableNotification);
+			throw e;
+		}
+	}
+
+	private async checkForDebugLspServer(): Promise<Port> {
+		const port = getServerPort();
+		if (port === undefined) {
+			return undefined;
+		}
+
+		return new Promise(resolve => {
+			const checkListen = () => {
+				const server = net.createServer();
+				server.on('error', () => {
+					resolve(port);
+				});
+				server.on('listening', () => {
+					server.close();
+					resolve(undefined);
+				});
+				server.listen(port, '127.0.0.1');
+			};
+
+			if (this.usingDebugServer) {
+				// Wait for server to restart
+				setTimeout(checkListen, 2500);
+			} else {
+				checkListen();
+			}
+		});
+	}
+
+	private installErrorHandler() {
+		const placeholder = this.clientOptions
+			.errorHandler as PlaceholderErrorHandler;
+		placeholder.delegate = new EnvdErrorHandler(this, maxRestartCount);
+	}
 }

f3ed3007dfe58120c4ae1316b1cd4dd0e790fdcc
cutecutecat <starkind1997@gmail.com>
2023-01-11 11:16:42 +0800

    register sidebar and command
    Signed-off-by: cutecutecat <starkind1997@gmail.com>

--- src/extension.ts
+++ src/extension.ts
@@ -12,28 +12,111 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { ExtensionContext, window } from "vscode"
-// import { TiltfileErrorWatcher } from "./tiltfile-error-watcher"
-import { EnvdLspClient } from "./envd-lsp-client"
+import {type ExtensionContext, window, commands} from 'vscode';
+import {EnvdLspClient} from './envd-lsp-client';
+import {envdChannel, Module, warn} from './logger';
+import {attachSSH, attachWindow, askDestroyEnvironment, askRemoveImage, askRemoveContext, envdUseContext, attachEndpoint} from './terminal-ops';
+import {type ContextCurrent, type ContextOther} from './sidebar/context-entry';
+import {type Environment} from './sidebar/env-entry';
+import {type Image} from './sidebar/img-entry';
+import {type EndPointEntry} from './sidebar/shared-entry';
+import {CtxProvider} from './sidebar/sidebar-context';
+import {EnvProvider} from './sidebar/sidebar-env';
+import {ImgProvider} from './sidebar/sidebar-img';
 
-const extensionName = "envd"
-
-let client: EnvdLspClient
-// let tiltfileErrorWatcher: TiltfileErrorWatcher
+let client: EnvdLspClient;
 
 export function activate(context: ExtensionContext) {
-  const ch = window.createOutputChannel(extensionName)
-  ch.append("???")
-  client = new EnvdLspClient(context, ch)
-  client.start()
-  // tiltfileErrorWatcher = new TiltfileErrorWatcher(context, ch)
-  // tiltfileErrorWatcher.start()
-  // addEnvdLinkToStatusBar(context)
+	client = new EnvdLspClient(context, envdChannel);
+	client.start();
+
+	const envProvider = new EnvProvider();
+	const imgProvider = new ImgProvider();
+	const ctxProvider = new CtxProvider();
+	window.registerTreeDataProvider('envd-environment', envProvider);
+	window.registerTreeDataProvider('envd-image', imgProvider);
+	window.registerTreeDataProvider('envd-context', ctxProvider);
+
+	commands.registerCommand('envd-environment.refresh', () => {
+		envProvider.refresh();
+	});
+	commands.registerCommand('envd-image.refresh', () => {
+		imgProvider.refresh();
+	});
+	commands.registerCommand('envd-context.refresh', () => {
+		ctxProvider.refresh();
+	});
+
+	commands.registerCommand('environment.attach-ssh', (node: Environment) => {
+		const info = envProvider.getEnvInfo(node.label as string);
+		if (!info) {
+			warn('Get environment infomation failed', Module.MANAGER);
+			return;
+		}
+
+		attachSSH(info);
+	});
+	commands.registerCommand('environment.attach-window', (node: Environment) => {
+		const info = envProvider.getEnvInfo(node.label as string);
+		if (!info) {
+			warn('Get environment infomation failed', Module.MANAGER);
+			return;
+		}
+
+		void attachWindow(info);
+	});
+	commands.registerCommand('endpoint.attach-endpoint', (node: EndPointEntry) => {
+		attachEndpoint(node.label as string);
+	});
+	commands.registerCommand('environment.destroy', async (node: Environment) => {
+		const info = envProvider.getEnvInfo(node.label as string);
+		if (!info) {
+			warn('Get environment infomation failed', Module.MANAGER);
+			return;
+		}
+
+		await askDestroyEnvironment(info);
+		envProvider.refresh();
+		imgProvider.refresh();
+	});
+
+	commands.registerCommand('image.remove', async (node: Image) => {
+		const info = imgProvider.getImgInfo(node.label as string);
+		if (!info) {
+			warn('Get image infomation failed', Module.MANAGER);
+			return;
+		}
+
+		await askRemoveImage(info);
+		imgProvider.refresh();
+	});
+
+	commands.registerCommand('context.use', async (node: ContextCurrent | ContextOther) => {
+		const info = ctxProvider.getCtxInfo(node.label as string);
+		if (!info) {
+			warn('Get context infomation failed', Module.MANAGER);
+			return;
+		}
+
+		await envdUseContext(info);
+		ctxProvider.refresh();
+	});
+	commands.registerCommand('context.remove', async (node: ContextCurrent | ContextOther) => {
+		const info = ctxProvider.getCtxInfo(node.label as string);
+		if (!info) {
+			warn('Get context infomation failed', Module.MANAGER);
+			return;
+		}
+
+		await askRemoveContext(info);
+		ctxProvider.refresh();
+	});
 }
 
 export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined
-  }
-  return client.stop()
+	if (!client) {
+		return undefined;
+	}
+
+	return client.stop();
 }
